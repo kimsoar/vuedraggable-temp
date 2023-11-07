cf8fa1140fab58095c15e975fe6a7953cefcee4f
kimsoar <kimsoar17@gmail.com>
2023-11-07 20:04:36 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="row">
+    <div class="col-3">
+      <h3>Draggable 1</h3>
+      <draggable
+        id="left"
+        :sort="false"
+        class="dragArea list-group"
+        :list="list1"
+        :group="{ name: 'people', pull: 'clone', put: false }"
+        @unchoose="unchoose"
+        :move="move"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list1"
+          :key="element.name"
+        >
+        <div class="item">
+          <button :disabled="checkDisable(element)" @click="plusClick(element)">+</button>
+          {{ element.name }}
+        </div>
+        </div>
+      </draggable>
+    </div>
+
+    <div class="col-3">
+      <h3>Draggable 2</h3>
+      <draggable
+        id="right"
+        :sort="false"
+        class="dragArea list-group"
+        :list="list2"
+        group="people"
+        @add="add"
+        @unchoose="unchoose"
+        :move="move"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list2"
+          :key="element.name"
+        >
+        <div class="item">
+          <button @click="minusClick(element)">-</button>
+          {{ element.name }}
+        </div>
+        </div>
+      </draggable>
+    </div>
+    <button @click="click">click</button>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import draggable from "vuedraggable";
+import _ from 'underscore';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+export default {
+  name: "clone-test",
+  display: "Clone",
+  order: 2,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list1: [
+        { name: "John", id: 1 },
+        { name: "Joao", id: 2 },
+        { name: "Jean", id: 3 },
+        { name: "Gerard", id: 4 }
+      ],
+      list2: [
+        { name: "John", id: 1 },
+      ]
+    };
+  },
+  methods: {
+    checkDisable(element) {
+      const find = _.find(this.list2, (item) => item.id === element.id);
+
+      return find !== undefined;
+    },
+    plusClick(item) {
+      console.log(item);
+      this.list2.push(item);
+    },
+    minusClick(item) {
+      this.list2 = _.without(this.list2, _.findWhere(this.list2, {id: item.id}));
+    },
+    add(evt) {// eslint-disable-line no-unused-vars
+      console.log('add');
+      this.list2 = _.sortBy(this.list2, "id");
+    },
+    unchoose(evt) { // eslint-disable-line no-unused-vars
+      console.log('unchoose');
+      const element = evt.originalEvent.target.closest('#right');
+      console.log(element);
+      if (evt.from.id === 'right' && element === null) {
+        console.log('제거요청')
+        console.log(evt.item._underlying_vm_.id, ' !!!!!!!!!');
+        this.list2 = _.without(this.list2, _.findWhere(this.list2, {id: evt.item._underlying_vm_.id}));
+      }
+    },
+    move(evt, originalEvent) { // eslint-disable-line no-unused-vars
+      const find = _.find(this.list2, (item) => item.id === evt.draggedContext.element.id);
+      return find === undefined;
+    },
+    click() {
+      console.log("list1: ",this.list1);
+      console.log("list2: ",this.list2);
+    },
+    findElement(element) {
+      element.closest('div');
+    }
+  }
+};
+</script>
+<style type="scss" scoped>
+.row {
+  font-size: 20px;
+  display: flex;
+
+  .dragArea {
+    height: 100%;
+    background-color: yellow;
+  }
+
+  .col-3 {
+    width: 300px;
+    margin: 5px;
+    border: 1px solid green;
+  }
+
+  .item {
+    border: 1px solid black;
+    padding: 3px;
+    margin: 5px;
+  }
 }
 </style>
